a8d0b2dabfa959cb131990ca32786eb9308316fd
Kyle St-Hill <kylesthill77@gmail.com>
2016-09-21 19:26:42 -0400

    endof day controllers moved and encrypted notes removed

---
+++ www/js/controllers/AllFilesController.js
@@ -0,0 +1,18 @@
+app.controller('AllFilesCtrl', function($scope, $ionicPlatform, NoteStore, FileFactory){  
+  $scope.name = 
+  $scope.reordering =false;
+
+  /*$scope.remove = function(noteId){
+    NoteStore.remove(noteId);
+  };
+
+  $scope.move = function(note, fromIndex, toIndex){
+    
+    NoteStore.move(note, fromIndex, toIndex);
+  };
+
+  $scope.toggleReordering = function(){
+    $scope.reordering = !$scope.reordering;
+  };*/
+  
+});
