b78f8e674be73b8dc368a7651b45359844b827a9
Kyle St-Hill <kylesthill77@gmail.com>
2016-09-23 17:57:42 -0400

    trying to go simple

--- www/js/app.js
+++ www/js/app.js
@@ -26,104 +26,22 @@ app.config(function($stateProvider, $urlRouterProvider) {
     templateUrl: "templates/tabs/decrypted_tabs.html"
   })
 
-  
-
   /*The different home tab bars available*/
   .state('tabs.home', {
-    url: "/home",
-    views: {
-      'home-tab': {
-        templateUrl: "templates/home.html",
-        controller: 'HomeTabCtrl'
-      }
-    }
-  })
-  .state('tabs.device', {
-    url: "/device",
-    views: {
-      'home-tab': {
-        templateUrl: "templates/homepage/device.html",
-        controller: 'DeviceCtrl'
-      }
-    }
-  })
-  .state('tabs.about', {
-    url: "/about",
-    views: {
-      'home-tab': {
-        templateUrl: "templates/homepage/about.html"
-      }
-    }
-  })
-  .state('tabs.about_part2', {
-      url: "/about_part2",
-      views: {
-        'home-tab': {
-          templateUrl: "templates/homepage/about_part2.html"
-        }
-      }
-    })
-  .state('tabs.tutorial', {
-    url: "/tutorial",
-    views: {
-      'home-tab': {
-        templateUrl: "templates/homepage/tutorial.html"
-      }
-    }
-  })
-  .state('tabs.settings', {
-    url: "/settings",
-    views: {
-      'home-tab': {
-        templateUrl: "templates/homepage/settings.html"
-      }
-    }
-  })
-  .state('tabs.contact_us', {
-    url: "/contact_us",
-    views: {
-      'home-tab': {
-        templateUrl: "templates/homepage/contact_us.html"
-      }
-    }
+    url: "/home/:username",
+    templateUrl: "templates/home.html",
+    controller: 'HomeTabCtrl'
   })
 
+  .state('encrypt', {
+    url: "/encrypt/:username",
+    templateUrl: "templates/encrypt.html"
+  })
 
-  /*Home tab navigation bar*/
-  /*.state('tabs.encrypted', {
-    url: "/encrypted",
-    views: {
-      'encrypted-tab': {
-        templateUrl: "templates/videos.html"
-      }
-    }
+  .state('tabs.decrypt', {
+    url: "/decrypt/:username",
+    templateUrl: "templates/decrypt.html"
   })
-  .state('tabs.decrypted', {
-    url: "/decrypted",
-    views: {
-      'decrypted-tab': {
-        templateUrl: "templates/videos.html"
-      }
-    }
-  })*/
-  .state('tabs.all', {
-    url: "/all",
-    views: {
-      'all-files-tab': {
-        templateUrl: "templates/all.html",
-        controller: 'AllFilesCtrl'
-      }
-    }
-  })
-  /*
- .state('tabs.about', {
-    url: "/about",
-    views: {
-      'about-tab': {
-        templateUrl: "templates/homepage/about.html"
-      }
-    }
-  })*/
 
   /*Encrypted view tabs*/
   .state('encrypted_tabs.home', {
@@ -237,32 +155,46 @@ app.config(function($stateProvider, $urlRouterProvider) {
     }
   })
 
-  /*Other states from notes app*/
-  /*.state('list', {
-    url: '/list',
-    templateUrl: 'templates/list.html',
-    controller: 'ListCtrl'
-  })*/
-
-  /*.state('add', {
-    url: '/add',
-    templateUrl: 'templates/edit.html',
-    controller: 'AddCtrl'
-  })
-
-  .state('edit', {
-    url: '/edit/:noteId',
-    templateUrl: 'templates/edit.html',
-    controller: 'EditCtrl'
-  });*/
-
   $urlRouterProvider.otherwise('/login');
 });
 
-app.controller('HomeTabCtrl', function($scope) {
-  console.log('HomeTabCtrl');
-  //$state.go('tabs.home');
+app.controller('HomeTabCtrl', function($scope, $stateParams) {
+  $scope.username = $stateParams.username;
+
+  $scope.Encrypt = function(){
+  /*  if(username){
+    $state.go('tabs.encrypt',{username: username});    
+  }*/
+  }
+  $scope.Decrypt = function(){
+   /* if(username){
+    $state.go('tabs.decrypt',{username: username});
+  }*/
+  }
+  
 });
+
+app.controller('TabEncryptCtrl', function($scope, $state, $stateParams) {
+  $scope.username = $stateParams.username;
+
+  $scope.BrowseDecryptedFiles = function(){
+    if(username){
+    $scope.show ="Encrypt" ; 
+  }
+  }
+  
+});
+
+app.controller('TabDecryptCtrl', function($scope, $state,  $stateParams) {
+  $scope.username = $stateParams.username;
+
+  $scope.BrowseEncryptedFiles = function(){
+    if(username){
+    $scope.show ="Decrypt" ;
+  }
+  }
+});
+
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- www/js/controllers/LoginController.js
+++ www/js/controllers/LoginController.js
@@ -1,9 +1,9 @@
 app.controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state){  
     $scope.user = {};
- 
-    $scope.login = function() {
+
+    $scope.login = function(username) {
         LoginService.loginUser($scope.user.username, $scope.user.password).success(function(user) {
-             $state.go('tabs.home');
+             $state.go('tabs.home',{username: username});
         }).error(function(user) {
             var alertPopup = $ionicPopup.alert({
                 title: 'Login failed!',
